d16caf8e43f05fcdef369b0f9c8c88bf1e9ab591
cphsuan <cphsuan17@gmail.com>
2022-08-02 15:06:26 +0800

    organize Perspective Transform

--- LaneAF/infer_vil100.py
+++ LaneAF/infer_vil100.py
@@ -20,7 +20,7 @@ from models.enet.ENet import ENet
 from utils.affinity_fields import decodeAFs
 from utils.metrics import match_multi_class, LaneEval
 from utils.visualize import tensor2image, create_viz
-
+from tools.perspective_correction import *
 
 parser = argparse.ArgumentParser('Options for inference with LaneAF models in PyTorch...')
 parser.add_argument('--input_video', type=str , default='/media/hsuan/data/VIL100/videos/0_Road014_Trim004_frames.avi', help='path to input video')
@@ -56,7 +56,7 @@ if args.cuda:
 kwargs = {'batch_size': args.batch_size, 'shuffle': False, 'num_workers': 1}
 
 
-class parameter:
+class Parm:
     def __init__(self,IMG_H, IMG_W):
         self.IMG_H = IMG_H
         self.IMG_org_crop_H = IMG_H
@@ -147,170 +147,23 @@ def LaneAF(image, net):
     return seg_out, img_out
 
 
-def preprocessing(seg_out, img_out):
+def preprocessing(seg_out_LaneAF, img_out_LaneAF):
     """ preprocessing image : create centerlane → """
     #####test 建立lane class
     global LaneID
-    LaneID = []
-
-    ID = np.delete(np.unique(seg_out), [0]) #有幾條道路線
-
-    for i in ID:
-        cal_seg = seg_out.copy()
-        cal_seg[cal_seg != i] = 0
-        CenterPoints = []
-        AllPoints = []
-
-        for row in range(np.shape(cal_seg)[0]): #每條row去找
-            col_loc = []
-            for j, v in enumerate(cal_seg[row]):
-                if (v == i):
-                    col_loc.append(j)
-                    AllPoints.append([j,row])
-
-            if (np.isnan(np.median(col_loc)) == False):
-                CenterPoints.append([row, int(np.median(col_loc))]) #row的中心位置
-
-        #建立中心線
-        y, x = zip(*CenterPoints)
-
-        x_scale = [i*8 for i in x] #放大成原尺寸
-        y_scale = [i*8 for i in y]
-        equa = np.polyfit(x_scale, y_scale, 1) #建立線
-
-        #建立lane class
-        lane_name = "LaneID_"+str(i) 
-        LaneID.append((Lane(lane_name,equa,np.array(AllPoints)*8)))
-    
+    LaneID = centerline(seg_out_LaneAF)    
     ####斜率判斷
-    left_loc=[]
-    right_loc=[]
-    for i, id in enumerate(LaneID):
-        slope = id.equa[0]
-
-        if slope < 0: #因為opencv座標關係
-            left_loc.append([id.name,id.equa])
-        else :
-            right_loc.append([id.name,id.equa])
-
-    lane_loc = {"leftmost":min(left_loc),"left_near_center":max(left_loc),"right_near_center":min(right_loc),"rightmost":max(right_loc)}
-
+    lane_loc = lane_loc_f(LaneID)
     ## 消失點
     Vpoint = vanishing_point(lane_loc)
-
-    
-    ### 顯示中心線 ###
-    # img_out2 = img_out.copy()
-    # for i in centerLane.keys():
-    #     f1 = centerLane[i]
-    #     # 延長線，找交點(visualize)
-    #     xvals = np.linspace(1, IMG_H, 20)
-    #     yvals = np.polyval(f1, xvals)
-    #     # 畫線
-    #     cv2.polylines(img_out2, np.int32([np.array(
-    #         list(zip(xvals, yvals.astype(int))))]), isClosed=False, color=(255, 255, 255), thickness=3)
-
-
     ### 透視變換 ###
-    img_out2, warped = perspective_transform(Vpoint,lane_loc, img_out)
-    
-    img = cv2.hconcat([img_out2, warped])  # 水平拼接
-    
+    img_out, warped, pm= perspective_transform(parm, LaneID, Vpoint, lane_loc, img_out_LaneAF)
 
-    return img
-
-class Lane:
-    def __init__(self,lane_name,equa,allpoints):
-        self.name = lane_name
-        self.equa = equa #中心線
-        self.allpoints = allpoints #所有點(已經放大) 
-
-    def print(self):
-        print("equa =", self.equa)
-        print("allpoints =", self.allpoints)
-    
-    @property
-    def min_axis_x(self):
-        return self.allpoints.min(axis=0)
-
-    @property
-    def max_axis_x(self):
-        return self.allpoints.max(axis=0)
-
-def vanishing_point(lane_loc):
-    """ 找消失點(left_near_center,right_near_center) """
-    P_diff = np.polysub(lane_loc["left_near_center"][1], lane_loc["right_near_center"][1])
-    Vx = np.roots(P_diff)
-    Vy = np.polyval(lane_loc["left_near_center"][1], Vx)
-    Vpoint = np.append(Vx, Vy)
-    return Vpoint
-
-def perspective_transform(Vpoint,lane_loc, img_out):
-    """ 透視變換 """
-    # 計算投影Y軸
-    ProjY = int((parm.IMG_W-Vpoint[1])*0.25+Vpoint[1])
-    # 取 left_near_center right_near_center Vertical line
-    lane1x_u = int((ProjY - lane_loc["left_near_center"][1][1]) / lane_loc["left_near_center"][1][0]) 
-    lane2x_u = int((ProjY - lane_loc["right_near_center"][1][1]) / lane_loc["right_near_center"][1][0]) 
-    lane1x_d = int((parm.IMG_W - lane_loc["left_near_center"][1][1]) / lane_loc["left_near_center"][1][0]) 
-    lane2x_d = int((parm.IMG_W - lane_loc["right_near_center"][1][1]) / lane_loc["right_near_center"][1][0])
-    # 原點
-    srcPts = np.float32([(lane1x_u, int(ProjY)),(lane2x_u, int(ProjY)),(lane1x_d, parm.IMG_W), (lane2x_d, parm.IMG_W)]) #(左上 右上 右下 左下)
-    # src points
-    img_out2 = img_out.copy()
-    # cv2.circle(img_out2, (lane1x_u, int(ProjY)),10, (255, 255, 0), 4)
-    # cv2.circle(img_out2, (lane2x_u, int(ProjY)),10, (255, 255, 0), 4)
-    # cv2.circle(img_out2, (lane1x_d, IMG_W),10, (255, 255, 0), 4)
-    # cv2.circle(img_out2, (lane2x_d, IMG_W),10, (255, 255, 0), 4)
-    # 投影點
-    dstPts = np.float32([(lane1x_d, 0), (lane2x_d, 0),(lane1x_d, parm.IMG_W), (lane2x_d, parm.IMG_W)])
-    # 透視變換矩陣
-    M = cv2.getPerspectiveTransform(srcPts, dstPts+100)
-    warped = cv2.warpPerspective(img_out2, M, (5000, 5000), flags=cv2.INTER_LINEAR)
-    
-    # #計算擷取位置
-    # crop_x =[]
-    # for id in (LaneID):
-    #     x = int((ProjY - id.equa[1]) / id.equa[0]) #計算lane 在投影Y軸上的X值
-    #     dst= corresponding_coordinates((int(x), int(ProjY)),M)
-    #     crop_x.append(dst[0])
-    
-    # print(min(crop_x)-100, max(crop_x)+100)
-    crop_loc(ProjY,M)
+    crop_loc(pm[0],pm[1],LaneID,parm)
     cropimage = warped[0:int(parm.IMG_W) , parm.crop[0] : parm.crop[1]]
 
-    img_out2 = img_out.copy()
-    # horizontal line
-    cv2.line(img_out2, (0, int(Vpoint[1])), (int(parm.IMG_H), int(Vpoint[1])),color=(0, 0, 255), thickness=3)
-    # Vertical line
-    cv2.line(img_out2, (int(Vpoint[0]), int(Vpoint[1])), (int(Vpoint[0]), parm.IMG_W),color=(0, 0, 255), thickness=3)
-    # ProjY horizontal line
-    cv2.line(img_out2, (0, ProjY), (int(parm.IMG_H), ProjY),color=(0, 255, 0), thickness=3)
-    # src points
-    cv2.circle(img_out2, (lane1x_u, int(ProjY)),10, (255, 0, 0), 4)
-    cv2.circle(img_out2, (lane2x_u, int(ProjY)),10, (255, 0, 0), 4)
-    cv2.circle(img_out2, (lane1x_d, int(parm.IMG_W)),10, (255, 0, 0), 4)
-    cv2.circle(img_out2, (lane2x_d, int(parm.IMG_W)),10, (255, 0, 0), 4)
-    return img_out2, cropimage
-
-def corresponding_coordinates(pos,M):
-    """透視變換 每個座標對應"""
-    u = pos[0]
-    v = pos[1]
-    x = (M[0][0]*u+M[0][1]*v+M[0][2])/(M[2][0]*u+M[2][1]*v+M[2][2])
-    y = (M[1][0]*u+M[1][1]*v+M[1][2])/(M[2][0]*u+M[2][1]*v+M[2][2])
-    return (int(x), int(y))
-
-def crop_loc(ProjY,M):
-    #計算擷取位置
-    crop_x =[]
-    for id in (LaneID):
-        x = int((ProjY - id.equa[1]) / id.equa[0]) #計算lane 在投影Y軸上的X值
-        dst= corresponding_coordinates((int(x), int(ProjY)),M)
-        crop_x.append(dst[0])
-    parm.crop = (round(min(crop_x)-200,-2), round(max(crop_x)+200,-2))
-
-
+    img = cv2.hconcat([img_out, cropimage])  # 水平拼接
+    return img
 
 if __name__ == "__main__":
 
@@ -326,12 +179,12 @@ if __name__ == "__main__":
         model.cuda()
     print(model)
     
+
     """ preprocessing video  """
     cap = cv2.VideoCapture(args.input_video)
-    fps = cap.get(cv2.CAP_PROP_FPS)
+    
     # 寫入檔案
     fourcc = cv2.VideoWriter_fourcc(*"XVID")
-    # videoWrite = cv2.VideoWriter('/home/hsuan/result.avi', fourcc, fps, (4020, 1024))
 
     i = 1
     while (cap.isOpened()):
@@ -341,23 +194,51 @@ if __name__ == "__main__":
             print("Can't receive frame (stream end?). Exiting ...")
             break
         if i == 1:
-            parm = parameter(np.shape(frame)[1],np.shape(frame)[0])
+            parm = Parm(np.shape(frame)[1],np.shape(frame)[0])
             parm.frame_count = cap.get(cv2.CAP_PROP_FRAME_COUNT)
-            seg_out, img_out = LaneAF(frame, model)
-            img_out = preprocessing(seg_out, img_out)
+            parm.fps = cap.get(cv2.CAP_PROP_FPS)
 
-            # videoWrite = cv2.VideoWriter('/home/hsuan/result.avi', fourcc, fps, (4020, 1024))
-            videoWrite = cv2.VideoWriter('/home/hsuan/result.avi', fourcc, fps, (int(parm.IMG_H+parm.crop[1]-parm.crop[0]), int(parm.IMG_W)))
+            seg_out_LaneAF, img_out_LaneAF = LaneAF(frame, model)
+            img_out = preprocessing(seg_out_LaneAF, img_out_LaneAF)
+
+            ### 建立lane class ###
+            global LaneID
+            LaneID = centerline(seg_out_LaneAF)    
+            ### 斜率判斷 ###
+            lane_loc = lane_loc_f(LaneID)
+            ### 消失點 ###
+            Vpoint = vanishing_point(lane_loc)
+            ### 透視變換 ###
+            img_out, warped, pm= perspective_transform(parm, LaneID, Vpoint, lane_loc, img_out_LaneAF)
+            ### 建立剪裁參數 ###
+            crop_loc(pm[0],pm[1],LaneID,parm)
+            ### 剪裁圖片 ###
+            cropimage = warped[0:int(parm.IMG_W) , parm.crop[0] : parm.crop[1]]
+            img = cv2.hconcat([img_out, cropimage])  # 水平拼接
+            
+            videoWrite = cv2.VideoWriter('/home/hsuan/result.avi', fourcc, parm.fps, (int(parm.IMG_H+parm.crop[1]-parm.crop[0]), int(parm.IMG_W)))
+            videoWrite.write(img)
 
 
-
-        seg_out, img_out = LaneAF(frame, model)
-        img_out = preprocessing(seg_out, img_out)
-        # cv2.imshow("img_out", img_out)
+        seg_out_LaneAF, img_out_LaneAF = LaneAF(frame, model)
+        
+        ### 建立lane class ###
+        LaneID = centerline(seg_out_LaneAF)    
+        ### 斜率判斷 ###
+        lane_loc = lane_loc_f(LaneID)
+        ### 消失點 ###
+        Vpoint = vanishing_point(lane_loc)
+        ### 透視變換 ###
+        img_out, warped, pm= perspective_transform(parm, LaneID, Vpoint, lane_loc, img_out_LaneAF)
+        ### 剪裁圖片 ###
+        cropimage = warped[0:int(parm.IMG_W) , parm.crop[0] : parm.crop[1]]
+        img = cv2.hconcat([img_out, cropimage])  # 水平拼接
+        
+        # cv2.imshow("img_out", img)
         # cv2.waitKey(0)
-        videoWrite.write(img_out)
+        videoWrite.write(img)
 
-        print('Done with image {} out of {}...'.format(i, parm.frame_count))
+        print('Done with image {} out of {}...'.format(i, int(parm.frame_count)))
         i+=1
 
     videoWrite.release()
--- LaneAF/tools/perspective_correction.py
+++ LaneAF/tools/perspective_correction.py
@@ -1,3 +1,129 @@
 import numpy as np
 import cv2
 
+class Lane:
+    def __init__(self,lane_name,equa,allpoints):
+        self.name = lane_name
+        self.equa = equa #中心線
+        self.allpoints = allpoints #所有點(已經放大) 
+
+    def print(self):
+        print("equa =", self.equa)
+        print("allpoints =", self.allpoints)
+    
+    @property
+    def min_axis_x(self):
+        return self.allpoints.min(axis=0)
+
+    @property
+    def max_axis_x(self):
+        return self.allpoints.max(axis=0)
+
+def centerline(seg_out):
+    LaneID = []
+    ID = np.delete(np.unique(seg_out), [0]) #有幾條道路線
+
+    for i in ID:
+        cal_seg = seg_out.copy()
+        cal_seg[cal_seg != i] = 0
+        CenterPoints = []
+        AllPoints = []
+
+        for row in range(np.shape(cal_seg)[0]): #每條row去找
+            col_loc = []
+            for j, v in enumerate(cal_seg[row]):
+                if (v == i):
+                    col_loc.append(j)
+                    AllPoints.append([j,row])
+
+            if (np.isnan(np.median(col_loc)) == False):
+                CenterPoints.append([row, int(np.median(col_loc))]) #row的中心位置
+
+        #建立中心線
+        y, x = zip(*CenterPoints)
+
+        x_scale = [i*8 for i in x] #放大成原尺寸
+        y_scale = [i*8 for i in y]
+        equa = np.polyfit(x_scale, y_scale, 1) #建立線
+
+        #建立lane class
+        lane_name = "LaneID_"+str(i) 
+        LaneID.append((Lane(lane_name,equa,np.array(AllPoints)*8)))
+    
+    return LaneID
+
+def lane_loc_f(LaneID):
+    left_loc=[]
+    right_loc=[]
+    for id in LaneID:
+        slope = id.equa[0]
+
+        if slope < 0: #因為opencv座標關係
+            left_loc.append([id.name,id.equa])
+        else :
+            right_loc.append([id.name,id.equa])
+
+    lane_loc = {"leftmost":min(left_loc),"left_near_center":max(left_loc),"right_near_center":min(right_loc),"rightmost":max(right_loc)}
+    return lane_loc
+
+def vanishing_point(lane_loc):
+    """ 找消失點(left_near_center,right_near_center) """
+    P_diff = np.polysub(lane_loc["left_near_center"][1], lane_loc["right_near_center"][1])
+    Vx = np.roots(P_diff)
+    Vy = np.polyval(lane_loc["left_near_center"][1], Vx)
+    Vpoint = np.append(Vx, Vy)
+    return Vpoint
+
+def corresponding_coordinates(pos,M):
+    """透視變換 每個座標對應"""
+    u = pos[0]
+    v = pos[1]
+    x = (M[0][0]*u+M[0][1]*v+M[0][2])/(M[2][0]*u+M[2][1]*v+M[2][2])
+    y = (M[1][0]*u+M[1][1]*v+M[1][2])/(M[2][0]*u+M[2][1]*v+M[2][2])
+    return (int(x), int(y))
+
+def perspective_transform(parm, LaneID, Vpoint, lane_loc, img_out):
+    """ 透視變換 """
+    # 計算投影Y軸
+    ProjY = int((parm.IMG_W-Vpoint[1])*0.25+Vpoint[1])
+    # 取 left_near_center right_near_center Vertical line
+    lane1x_u = int((ProjY - lane_loc["left_near_center"][1][1]) / lane_loc["left_near_center"][1][0]) 
+    lane2x_u = int((ProjY - lane_loc["right_near_center"][1][1]) / lane_loc["right_near_center"][1][0]) 
+    lane1x_d = int((parm.IMG_W - lane_loc["left_near_center"][1][1]) / lane_loc["left_near_center"][1][0]) 
+    lane2x_d = int((parm.IMG_W - lane_loc["right_near_center"][1][1]) / lane_loc["right_near_center"][1][0])
+    # 原點
+    srcPts = np.float32([(lane1x_u, int(ProjY)),(lane2x_u, int(ProjY)),(lane1x_d, parm.IMG_W), (lane2x_d, parm.IMG_W)]) #(左上 右上 右下 左下)
+    # src points
+    img_out2 = img_out.copy()
+    # cv2.circle(img_out2, (lane1x_u, int(ProjY)),10, (255, 255, 0), 4)
+    # cv2.circle(img_out2, (lane2x_u, int(ProjY)),10, (255, 255, 0), 4)
+    # cv2.circle(img_out2, (lane1x_d, IMG_W),10, (255, 255, 0), 4)
+    # cv2.circle(img_out2, (lane2x_d, IMG_W),10, (255, 255, 0), 4)
+    # 投影點
+    dstPts = np.float32([(lane1x_d, 0), (lane2x_d, 0),(lane1x_d, parm.IMG_W), (lane2x_d, parm.IMG_W)])
+    # 透視變換矩陣
+    M = cv2.getPerspectiveTransform(srcPts, dstPts+100)
+    warped = cv2.warpPerspective(img_out2, M, (5000, 5000), flags=cv2.INTER_LINEAR)
+    
+    img_out2 = img_out.copy()
+    # horizontal line
+    cv2.line(img_out2, (0, int(Vpoint[1])), (int(parm.IMG_H), int(Vpoint[1])),color=(0, 0, 255), thickness=3)
+    # Vertical line
+    cv2.line(img_out2, (int(Vpoint[0]), int(Vpoint[1])), (int(Vpoint[0]), parm.IMG_W),color=(0, 0, 255), thickness=3)
+    # ProjY horizontal line
+    cv2.line(img_out2, (0, ProjY), (int(parm.IMG_H), ProjY),color=(0, 255, 0), thickness=3)
+    # src points
+    cv2.circle(img_out2, (lane1x_u, int(ProjY)),10, (255, 0, 0), 4)
+    cv2.circle(img_out2, (lane2x_u, int(ProjY)),10, (255, 0, 0), 4)
+    cv2.circle(img_out2, (lane1x_d, int(parm.IMG_W)),10, (255, 0, 0), 4)
+    cv2.circle(img_out2, (lane2x_d, int(parm.IMG_W)),10, (255, 0, 0), 4)
+    return img_out2, warped, (ProjY,M)
+
+def crop_loc(ProjY,M,LaneID,parm):
+    #計算擷取位置
+    crop_x =[]
+    for id in (LaneID):
+        x = int((ProjY - id.equa[1]) / id.equa[0]) #計算lane 在投影Y軸上的X值
+        dst= corresponding_coordinates((int(x), int(ProjY)),M)
+        crop_x.append(dst[0])
+    parm.crop = (round(min(crop_x)-200,-2), round(max(crop_x)+200,-2))
