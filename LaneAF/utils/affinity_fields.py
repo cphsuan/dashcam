8711004587ba6d9c44eef0af91e61ccd75d5ceac
cphsuan <cphsuan17@gmail.com>
2022-10-28 15:01:38 +0800

    new method for relane

--- LaneAF/utils/affinity_fields.py
+++ LaneAF/utils/affinity_fields.py
@@ -1,7 +1,9 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-
+from utils.visualize import create_viz
+import pysnooper
+#@pysnooper.snoop()
 
 def generateAFs(label, viz=False):
     # creating AF arrays
@@ -16,17 +18,17 @@ def generateAFs(label, viz=False):
         prev_row = label.shape[0]
 
         # parse row by row, from second last to first
-        for row in range(label.shape[0]-1, -1, -1):
+        for row in range(label.shape[0]-1, -1, -1): #由後往前
             cols = np.where(label[row, :] == l)[0] # get fg cols
 
-            # get horizontal vector
+            # get horizontal vector #HAF[row, c, 1] = 0
             for c in cols:
                 if c < np.mean(cols):
                     HAF[row, c, 0] = 1.0 # point to right
                 elif c > np.mean(cols):
                     HAF[row, c, 0] = -1.0 # point to left
                 else:
-                    HAF[row, c, 0] = 0.0 # point to left
+                    HAF[row, c, 0] = 0.0 # point
 
             # check if both previous cols and current cols are non-empty
             if prev_cols.size == 0: # if no previous row/cols, update and continue
@@ -53,10 +55,12 @@ def generateAFs(label, viz=False):
     if viz: # visualization
         down_rate = 1 # downsample visualization by this factor
         fig, (ax1, ax2) = plt.subplots(1, 2)
-        # visualize VAF
+        # visualize VAF #quiver畫箭頭
+        ax1.title.set_text('VAF Plot')
         q = ax1.quiver(np.arange(0, label.shape[1], down_rate), -np.arange(0, label.shape[0], down_rate), 
             VAF[::down_rate, ::down_rate, 0], -VAF[::down_rate, ::down_rate, 1], scale=120)
         # visualize HAF
+        ax2.title.set_text('HAF Plot')
         q = ax2.quiver(np.arange(0, label.shape[1], down_rate), -np.arange(0, label.shape[0], down_rate), 
             HAF[::down_rate, ::down_rate, 0], -HAF[::down_rate, ::down_rate, 1], scale=120)
         plt.show()
@@ -67,11 +71,113 @@ def decodeAFs(BW, VAF, HAF, fg_thresh=128, err_thresh=5, viz=False):
     output = np.zeros_like(BW, dtype=np.uint8) # initialize output array
     lane_end_pts = [] # keep track of latest lane points
     next_lane_id = 1 # next available lane ID
+    # if viz:
+    #     im_color = cv2.applyColorMap(BW, cv2.COLORMAP_JET)
+    #     cv2.imshow('BW', im_color)
+    #     ret = cv2.waitKey(0)
+    
+    # start decoding from last row to first
+    for row in range(BW.shape[0]-1, -1, -1):
+        # print("row=",row)
+        cols = np.where(BW[row, :] > fg_thresh)[0] # get fg cols #col location
+        # print("cols",cols)
+        clusters = [[]]
 
+        if cols.size > 0:
+            prev_col = cols[0]
+
+        # parse horizontally
+        for col in cols:
+
+            if HAF[row, prev_col] <= 0 and HAF[row, col] > 0: # found lane end, spawn new lane
+                clusters.append([])
+                clusters[-1].append(col)
+                prev_col = col
+            else: # keep moving to the right
+
+                if col - prev_col > err_thresh: # if too far away from last point
+                    for i in range(1,col - prev_col):
+                        if i == 20:
+                            break
+                        if HAF[row, prev_col+i] <= 0:
+                            clusters[-1] += [prev_col+j for j in range(1,i+1)]
+                            break
+
+                    clusters.append([])
+                    clusters[-1].append(col)
+                    prev_col = col
+                    continue
+
+                clusters[-1].append(col)
+                prev_col = col
+        # parse vertically
+        # assign existing lanes
+        assigned = [False for _ in clusters]
+        C = np.Inf*np.ones((len(lane_end_pts), len(clusters)), dtype=np.float64)
+        # print(row,lane_end_pts) #lane_end_pts=y
+        # print(np.sort(lane_end_pts,axis=0))
+        current_mean_pts = []
+        for r, pts in enumerate(lane_end_pts): 
+            current_mean_pts.append(np.mean(pts,axis=0))
+
+        # for c, cluster in enumerate(clusters):
+        #     if len(cluster) == 0:
+        #         continue
+        #     cluster_mean = np.array([[np.mean(cluster), row]], dtype=np.float32)
+        #     if cluster_mean[0]
+        
+        for r, pts in enumerate(lane_end_pts): # for each end point in an active lane
+            for c, cluster in enumerate(clusters):
+                if len(cluster) == 0:
+                    continue
+                # mean of current cluster
+                cluster_mean = np.array([[np.mean(cluster), row]], dtype=np.float32)
+
+                # get vafs from lane end points
+                vafs = np.array([VAF[int(round(x[1])), int(round(x[0])), :] for x in pts], dtype=np.float32)
+                vafs = vafs / np.linalg.norm(vafs, axis=1, keepdims=True)
+                # get predicted cluster center by adding vafs
+                pred_points = pts + vafs*np.linalg.norm(pts - cluster_mean, axis=1, keepdims=True)
+                # get error between prediceted cluster center and actual cluster center
+                error = np.mean(np.linalg.norm(cluster_mean - pred_points , axis=1))
+                C[r, c] = error
+        # assign clusters to lane (in acsending order of error)
+        row_ind, col_ind = np.unravel_index(np.argsort(C, axis=None), C.shape)
+
+        for r, c in zip(row_ind, col_ind):
+            if C[r, c] >= err_thresh:
+                break
+            if assigned[c]:
+                continue
+            assigned[c] = True
+            # update best lane match with current pixel
+            output[row, clusters[c]] = r+1
+            lane_end_pts[r] = np.stack((np.array(clusters[c], dtype=np.float32), row*np.ones_like(clusters[c])), axis=1)
+        
+        # initialize unassigned clusters to new lanes
+        for c, cluster in enumerate(clusters):
+            if len(cluster) == 0:
+                continue
+            if not assigned[c]:
+                output[row, cluster] = next_lane_id
+                lane_end_pts.append(np.stack((np.array(cluster, dtype=np.float32), row*np.ones_like(cluster)), axis=1))
+                next_lane_id += 1
     if viz:
-        im_color = cv2.applyColorMap(BW, cv2.COLORMAP_JET)
-        cv2.imshow('BW', im_color)
+        im_color = cv2.applyColorMap(40*output, cv2.COLORMAP_JET)
+        cv2.imshow('Output', im_color)
         ret = cv2.waitKey(0)
+
+    return output
+
+def  decodeAFs_0(BW, VAF, HAF, fg_thresh=128, err_thresh=5, viz=False): #原本的
+    output = np.zeros_like(BW, dtype=np.uint8) # initialize output array
+    lane_end_pts = [] # keep track of latest lane points
+    next_lane_id = 1 # next available lane ID
+
+    # if viz:
+    #     im_color = cv2.applyColorMap(BW, cv2.COLORMAP_JET)
+    #     cv2.imshow('BW', im_color)
+    #     ret = cv2.waitKey(0)
 
     # start decoding from last row to first
     for row in range(BW.shape[0]-1, -1, -1):
@@ -141,6 +247,7 @@ def decodeAFs(BW, VAF, HAF, fg_thresh=128, err_thresh=5, viz=False):
                 output[row, cluster] = next_lane_id
                 lane_end_pts.append(np.stack((np.array(cluster, dtype=np.float32), row*np.ones_like(cluster)), axis=1))
                 next_lane_id += 1
+    # input()
 
     if viz:
         im_color = cv2.applyColorMap(40*output, cv2.COLORMAP_JET)
